61d921ce3286b96d09b02507ecb280dc26fc0d50
mateoplana <mateoplana22@gmail.com>
2023-01-20 11:35:46 -0300

    mensajes en proceso

--- AppUsers/models.py
+++ AppUsers/models.py
@@ -1,8 +1,12 @@
 from django.db import models
+from django.conf import settings
 from django.utils import timezone
 from django.contrib.auth.models import User
 from PIL import Image
+import uuid
+from django.db.models import Count
 
+User=settings.AUTH_USER_MODEL
 
 # Create your models here.
 
@@ -30,6 +34,41 @@ class Perfil(models.Model):
             new_img = (100, 100)
             img.thumbnail(new_img)
             img.save(self.avatar.path)
+
+class ModeloBase(models.Model):
+    id=models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False)
+    tiempo=models.DateTimeField(auto_now_add=True)
+    actualizar=models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class CanaldeMensaje(ModeloBase):
+    canal=models.ForeignKey("Canal", on_delete=models.CASCADE)
+    usuario=models.ForeignKey(User, on_delete=models.CASCADE)
+    texto=models.TextField()
+
+class CanaldeUsuario(ModeloBase):
+    canal=models.ForeignKey("Canal", null=True , on_delete=models.SET_NULL)
+    usuario=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class CanalQuerySet(models.QuerySet):
+    def solo_uno(self):
+        return self.annotate(numero_usuarios=Count("usuarios").filter(numero_usuarios=1))
+
+    def solo_dos_u(self):
+        return self.annotate(numero_usuarios=Count("usuarios").filter(numero_usuarios=2))
+
+
+class CanalManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return CanalQuerySet(self.model, using=self._db)
+
+
+class Canal(ModeloBase):
+    usuarios=models.ManyToManyField(User, blank=True, through=CanaldeUsuario)
+    objects=CanalManager()
 
 
 
--- AppUsers/tests.py
+++ AppUsers/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
+from .models import CanaldeMensaje, CanaldeUsuario, Canal
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 # Create your tests here.
+
+class CanalTestCase(TestCase):
+    def setUp(self):
+        self.usuario_1=User.objects.create(username='juju1', password='asdasd1')
+        self.usuario_2=User.objects.create(username='juju2', password='asdasd2')
+        self.usuario_3=User.objects.create(username='juju3', password='asdasd3')
+
+    def test_usuario_count(self):
+        qs=User.Objetcs.all()
+        self.assertEqual(qs.count(), 3)
+
+    def test_cada_canaldeusuario(self):
+        qs=User.Objetcs.all()
+        for usuario in qs:
+            canal_obj=Canal.objects.create
+            canal_obj.usuarios.add(usuario)
+        canal_qs=Canal.objects.all()
+        self.assertEqual(canal_qs.count(), 3)
+        canal_qs_1=canal_qs.solo_uno()
+        self.assertEqual(canal_qs_1.count(), canal_qs.count())
+
+    def prueba_dos_usuarios(self):
+        canal_obj=Canal.objects.create()
+        CanaldeUsuario.objects.create(usuario=self.usuario_1, canal=canal_obj)
+        CanaldeUsuario.objects.create(usuario=self.usuario_2, canal=canal_obj)
+
+        canal_obj2=Canal.objects.create()
+        CanaldeUsuario.objects.create(usuario=self.usuario_3, canal=canal_obj2)
+
+        qs=Canal.objects.all()
+        self.assertEqual(qs.count(), 2)
+        solo_dos=qs.solo_dos()
+        self.assertEqual(solo_dos.count(), 1)
+
